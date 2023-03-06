b43c810f686f0007a927c9c1e3783aa72c11bbcd
Bruno Prestes <brunoasprestes@gmail.com>
2023-03-06 14:03:07 -0300

    ajustes na cor da fonte do botao cta

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,24 +15,20 @@ const Hero = () => {
         {/* <div className="invisible md:visible absolute top-0 right-0 max-w-[700px] w-52 lg:w-72 m-auto h-full">
           <CardExperience />
         </div> */}
+        <div className='absolute top-0 left-0 pt-5 pl-5 border-b-2 border-[#AA8068]'>
+            <h1 className='text-2xl text-[#AA8068] font-bold'>Dra. Renata Pamplona</h1>
+        </div>
 
-        <div id="sobre" className="flex absolute bottom-3 md:bottom-12 left-5 md:left-20 max-w-[350px] md:max-w-prose">
+        <div id="sobre" className="flex absolute bottom-3 md:bottom-12 left-5 md:left-16 max-w-[350px] md:max-w-prose">
           <div className="backdrop-opacity-10 rounded-2xl bg-[#EED3C6]/50 p-5 bg-opacity-60 shadow-2xl shadow-stone-500/50">
-            <h1 className="text-3xl uppercase lg:text-6xl text-[#AA8068]">Dra.</h1>
-            <h1 className="text-5xl uppercase lg:text-6xl font-bold text-[#AA8068]">
-              Renata
-              <br />
-              Pamplona
-            </h1>
+            <h2 className="text-2xl font-bold md:text-3xl lg:text-4xl text-[#AA8068]">Transforme seu sorriso com tratamentos odontológicos personalizados</h2>
 
             <hr className="w-full lg:w-full border mt-3 border-white/50" />
 
             <div className="my-5">
-              <h2 className="text-xl md:text-2xl text-[#AA8068] font-bold">
-                Sou Cirurgiã-Dentista, Odontolegista, Cirurgiã BucoMaxiloFacial e HABILITAÇÃO em Laserterapia
-              </h2>
+              
               <h2 className="text-lg md:text-xl text-[#AA8068]">
-                Atendimento 24 horas
+                Cuide da sua saúde bucal e melhore sua qualidade de vida - agende uma consulta hoje mesmo"
               </h2>
             </div>
 
