4cc976950d9602316ab3e6beb721d373ac26c5be
Bruno Prestes <brunoasprestes@gmail.com>
2023-03-08 20:49:12 -0300

    Atualização dos cards

--- src/components/Tratamentos.jsx
+++ src/components/Tratamentos.jsx
@@ -1,45 +1,134 @@
-import React from 'react'
-import implante from '../assets/implante.jpg'
-import ortodontia from '../assets/ortodontia.jpg'
-import protese from '../assets/protese.jpg'
-import faceta from '../assets/faceta.jpg'
-import clinica from '../assets/clinica.webp'
-import restauracao from '../assets/restauracao.webp'
-import bucomaxilo from '../assets/bucomaxilofacial.jpeg'
-import laserterapia from '../assets/laserterapia.jpg'
-import odonto from '../assets/odontopediatria.jpg'
+import React from "react";
+import implante from "../assets/implante.jpg";
+import ortodontia from "../assets/ortodontia.jpg";
+import protese from "../assets/protese.jpg";
+import faceta from "../assets/faceta.jpg";
+import clinica from "../assets/clinica.webp";
+import restauracao from "../assets/restauracao.webp";
+import bucomaxilo from "../assets/bucomaxilofacial.jpeg";
+import laserterapia from "../assets/laserterapia.jpg";
+import odonto from "../assets/odontopediatria.jpg";
+
+import { FaCheck } from 'react-icons/fa'
 
 const Tratamentos = () => {
   return (
     <div>
-        <div className='p-5'>
+      <div className="p-5">
+        <h2 className="text-4xl text-center font-bold text-[#7D513A]">
+          Tratamentos
+        </h2>
+        <h2 className="text-2xl font-bold text-center text-[#7D513A]/80 my-2">
+          Tudo o que você precisa, num só lugar!
+        </h2>
+        <h2 className="text-sm text-center text-[#7D513A]">
+          Não deixe que o medo do dentista impeça você de cuidar da sua saúde
+          bucal - Oferecemos tratamentos sem dor
+        </h2>
+      </div>
 
-            <h2 className='text-4xl text-center font-bold text-[#7D513A]'>
-                Tratamentos
-            </h2>
-            <h2 className='text-2xl font-bold text-center text-[#7D513A]/80 my-2'>
-                Tudo o que você precisa, num só lugar!
-            </h2>
-            <h2 className='text-sm text-center text-[#7D513A]'>
-                Não deixe que o medo do dentista impeça você de cuidar da sua saúde bucal - Oferecemos tratamentos sem dor
-            </h2>
+      <div className="p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="p-5 w-full">
+          <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+            <img
+              className="lg:h-72 md:h-48 w-full object-cover object-center"
+              src={implante}
+              alt="implante"
+            />
 
+            <div className="p-6 hover:bg-[#7D513A]/70 hover:text-white transition duration-300 ease-in">
+              <h2 className="text-base font-medium text-slate-900 mb-1">
+                Implante dentário
+              </h2>
+              <p className="leading-relaxed">
+                Não perca mais tempo sofrendo com problemas dentários. Agende
+                agora mesmo uma consulta para avaliação e descubra como o
+                implante dentário pode mudar a sua vida para melhor.
+              </p>
+              <div className="py-2">
+                <div className="flex items-center gap-2">
+                    <FaCheck />
+                    <p>Implante dentário unitário/multiplo</p>
+                </div>
+                <div className="flex items-center gap-2">
+                    <FaCheck />
+                    <p>Prótese dentária sobre implante</p>
+                </div>
+                <div className="flex items-center gap-2">
+                    <FaCheck />
+                    <p>Exerto ósseo</p>
+                </div>
+                <div className="flex items-center gap-2">
+                    <FaCheck />
+                    <p>Cirurgia de levantamento de seio maxilar</p>
+                </div>
+                <div className="flex items-center gap-2">
+                    <FaCheck />
+                    <p>Tratamento de gengivite e periodontite</p>
+                </div>
+              </div>
+              <div>
+                <button className="border-2 border-white bg-green-600 p-3 rounded-lg">
+                  <p className="text-white">Agende sua consulta</p>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div className='p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-            <div className='flex flex-col items-center justify-center bg-[#F3DDD2] rounded-xl p-5'>
-                <img
-                src={implante}
-                alt='implante dentário'
-                className='object-cover w-5/6 rounded-xl hover:translate-y-2 duration-500 shadow-lg shadow-slate-800/50' />
-                <h2 className='text-center text-xl mt-5 text-[#7D513A]'>Implante dentário</h2>
+        <div className="p-5 w-full">
+          <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+            <img
+              className="lg:h-72 md:h-48 w-full object-cover object-center"
+              src={ortodontia}
+              alt="ortodontia"
+            />
+
+            <div className="p-6 hover:bg-[#7D513A]/70 hover:text-white transition duration-300 ease-in">
+              <h2 className="text-base font-medium text-slate-900 mb-1">
+                Ortodontia
+              </h2>
+              <p className="leading-relaxed">
+                Não deixe que dentes tortos ou problemas de mordida afetem a sua confiança. 
+                Agende agora mesmo uma consulta para avaliação ortodôntica!
+              </p>
+              <div className="py-2">
+                <div className="flex items-center gap-2">
+                    <FaCheck />
+                    <p>Aparelhos ortodônticos fixos</p>
+                </div>
+                <div className="flex items-center gap-2">
+                    <FaCheck />
+                    <p>Aparelhos ortodônticos móveis</p>
+                </div>
+                <div className="flex items-center gap-2">
+                    <FaCheck />
+                    <p>Alinhadores transparentes</p>
+                </div>
+                <div className="flex items-center gap-2">
+                    <FaCheck />
+                    <p>Aparelhos ortopédicos</p>
+                </div>
+                <div className="flex items-center gap-2">
+                    <FaCheck />
+                    <p>Tratamentos acelerados</p>
+                </div>
+              </div>
+              <div>
+                <button className="border-2 border-white bg-green-600 p-3 rounded-lg">
+                  <p className="text-white">Agende sua consulta</p>
+                </button>
+              </div>
             </div>
-            <div className='flex flex-col items-center justify-center'>
+          </div>
+        </div>
+
+        {/* <div className='flex flex-col items-center justify-center bg-[#F3DDD2] rounded-xl p-5 shadow-lg shadow-slate-800/50'>
                 <img
                 src={ortodontia}
                 alt='ortodontia'
-                className='object-cover w-full rounded-lg hover:translate-y-2 duration-500' />
-                <h2 className='text-center my-3 text-xl text-[#7D513A]'>Ortodontia</h2>
+                className='object-cover w-5/6 rounded-xl hover:translate-y-2 duration-500 shadow-lg shadow-slate-800/50' />
+                <h2 className='text-center text-xl mt-5 text-[#7D513A] font-bold'>Ortodontia</h2>
             </div>
             <div className='flex flex-col items-center justify-center'>
                 <img
@@ -110,10 +199,10 @@ const Tratamentos = () => {
                 alt='HOF'
                 className='object-cover w-full rounded-lg hover:translate-y-2 duration-500' />
                 <h2 className='text-center my-3 text-xl text-[#AA8068]'>Harmonização Orofacial</h2>
-            </div>
-        </div>
+            </div> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Tratamentos
+export default Tratamentos;
