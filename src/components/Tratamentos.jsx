bef8f86f7c167bd390700653d69ac78a0fa86491
Bruno Prestes <brunoasprestes@gmail.com>
2023-03-16 17:30:23 -0300

    cards adicionados

--- src/components/Tratamentos.jsx
+++ src/components/Tratamentos.jsx
@@ -13,8 +13,8 @@ import { FaCheck } from "react-icons/fa";
 
 const Tratamentos = () => {
   return (
-    <div>
-      <div className="p-5">
+    <div className="flex flex-col justify-around items-center">
+      <div className="p-5 md:w-4/6">
         <h2 className="text-4xl text-center font-bold text-[#7D513A]">
           Tratamentos
         </h2>
@@ -23,11 +23,11 @@ const Tratamentos = () => {
         </h2>
         <h2 className="text-sm text-center text-[#7D513A]">
           Não deixe que o medo do dentista impeça você de cuidar da sua saúde
-          bucal - Oferecemos tratamentos sem dor
+          bucal - Oferecemos tratamentos sem dor!
         </h2>
       </div>
 
-      <div className="p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      <div className="md:w-4/6 p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         <div className="w-full">
           <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden hover:bg-[#7D513A]/70 hover:text-white transition duration-300 ease-in shadow-lg shadow-gray-400">
             <img
@@ -37,34 +37,36 @@ const Tratamentos = () => {
             />
 
             <div className="p-5">
-              <h2 className="text-xl font-medium text-slate-900 mb-1">
+              <h2 className="text-base font-medium text-slate-900 mb-1">
                 Implante dentário
               </h2>
-              <p className="leading-relaxed text-base">
+              <p className="leading-relaxed text-xs">
                 Não perca mais tempo sofrendo com problemas dentários. Agende
-                agora mesmo uma consulta para avaliação e descubra como o
-                implante dentário pode mudar a sua vida para melhor.
+                agora mesmo uma consulta e descubra como o implante dentário
+                pode mudar a sua vida para melhor.
               </p>
               <div className="py-2">
                 <div className="flex items-center gap-2">
-                  <FaCheck />
-                  <p className="text-sm my-1">Implante dentário unitário/multiplo</p>
+                  <FaCheck size={13} />
+                  <p className="text-xs my-1">
+                    Implante dentário unitário/múltiplo
+                  </p>
                 </div>
                 <div className="flex items-center gap-2">
-                  <FaCheck />
-                  <p className="text-sm my-1">Prótese dentária sobre implante</p>
+                  <FaCheck size={13} />
+                  <p className="text-xs my-1">
+                    Prótese dentária sobre implante
+                  </p>
                 </div>
                 <div className="flex items-center gap-2">
-                  <FaCheck />
-                  <p className="text-sm my-1">Exerto ósseo</p>
+                  <FaCheck size={13} />
+                  <p className="text-xs my-1">Enxerto ósseo</p>
                 </div>
                 <div className="flex items-center gap-2">
-                  <FaCheck />
-                  <p className="text-sm my-1">Cirurgia de levantamento de seio maxilar</p>
-                </div>
-                <div className="flex items-center gap-2">
-                  <FaCheck />
-                  <p className="text-sm my-1">Tratamento de gengivite e periodontite</p>
+                  <FaCheck size={13} />
+                  <p className="text-xs my-1">
+                    Cirurgia de levantamento de seio maxilar
+                  </p>
                 </div>
               </div>
             </div>
@@ -137,10 +139,10 @@ const Tratamentos = () => {
                 Prótese Dental
               </h2>
               <p className="leading-relaxed">
-                Sorrisos são a expressão da nossa alegria e felicidade. 
-                E quando um dente falha, pode ser difícil sorrir com confiança. 
-                Mas a prótese dentária pode devolver a você o sorriso que tanto deseja.
-                Agende uma consulta agora mesmo para avaliação.
+                Sorrisos são a expressão da nossa alegria e felicidade. E quando
+                um dente falha, pode ser difícil sorrir com confiança. Mas a
+                prótese dentária pode devolver à você o sorriso que tanto
+                deseja. Agende uma consulta agora mesmo.
               </p>
               <div className="py-2">
                 <div className="flex items-center gap-2">
@@ -153,19 +155,11 @@ const Tratamentos = () => {
                 </div>
                 <div className="flex items-center gap-2">
                   <FaCheck />
-                  <p className="text-sm my-1">Overdentures</p>
-                </div>
-                <div className="flex items-center gap-2">
-                  <FaCheck />
                   <p className="text-sm my-1">Coroas</p>
                 </div>
                 <div className="flex items-center gap-2">
                   <FaCheck />
                   <p className="text-sm my-1">Pontes fixas</p>
-                </div>
-                <div className="flex items-center gap-2">
-                  <FaCheck />
-                  <p className="text-sm my-1">Implantes dentários</p>
                 </div>
               </div>
             </div>
@@ -176,7 +170,6 @@ const Tratamentos = () => {
             </div>
           </div>
         </div>
-
         <div className="w-full">
           <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden hover:bg-[#7D513A]/70 hover:text-white transition duration-300 ease-in shadow-lg shadow-gray-400">
             <img
@@ -187,11 +180,12 @@ const Tratamentos = () => {
 
             <div className="p-5">
               <h2 className="text-xl font-medium text-slate-900 mb-1">
-                Lentes e Facetas
+                Lentes e Facetas (Estética)
               </h2>
               <p className="leading-relaxed">
-                Transforme seu sorriso em minutos! Tenha dentes perfeitos com nossas lentes e facetas de alta qualidade. 
-                Agende sua consulta agora mesmo!
+                Transforme seu sorriso em minutos! Tenha dentes perfeitos com
+                nossas lentes e facetas de alta qualidade. Agende sua consulta
+                agora mesmo!
               </p>
               <div className="py-2">
                 <div className="flex items-center gap-2">
@@ -205,10 +199,6 @@ const Tratamentos = () => {
                 <div className="flex items-center gap-2">
                   <FaCheck />
                   <p className="text-sm my-1">Clareamento dental</p>
-                </div>
-                <div className="flex items-center gap-2">
-                  <FaCheck />
-                  <p className="text-sm my-1">Alinhamento dentário</p>
                 </div>
                 <div className="flex items-center gap-2">
                   <FaCheck />
@@ -237,8 +227,9 @@ const Tratamentos = () => {
                 Clínica Geral
               </h2>
               <p className="leading-relaxed">
-                Tenha um sorriso saudável e radiante! Agende já sua consulta para cuidar da sua saúde bucal.
-                Oferecemos tratamentos personalizados e equipe especializada.
+                Tenha um sorriso saudável e radiante! Agende já sua consulta
+                para cuidar da sua saúde bucal. Oferecemos tratamentos
+                personalizados e equipe especializada.
               </p>
               <div className="py-2">
                 <div className="flex items-center gap-2">
@@ -247,19 +238,11 @@ const Tratamentos = () => {
                 </div>
                 <div className="flex items-center gap-2">
                   <FaCheck />
-                  <p className="text-sm my-1">Obturações</p>
+                  <p className="text-sm my-1">Restaurações</p>
                 </div>
                 <div className="flex items-center gap-2">
                   <FaCheck />
-                  <p className="text-sm my-1">Extrações dentárias</p>
-                </div>
-                <div className="flex items-center gap-2">
-                  <FaCheck />
-                  <p className="text-sm my-1">Tratamento de canal</p>
-                </div>
-                <div className="flex items-center gap-2">
-                  <FaCheck />
-                  <p className="text-sm my-1">Tratamento periodontal</p>
+                  <p className="text-sm my-1">Remoção de tártaro</p>
                 </div>
               </div>
             </div>
@@ -281,28 +264,22 @@ const Tratamentos = () => {
 
             <div className="p-5">
               <h2 className="text-xl font-medium text-slate-900 mb-1">
-                Restauração
+                Dentística
               </h2>
               <p className="leading-relaxed">
-                Recupere seu sorriso com nossos serviços de restauração! Dentes quebrados ou com cáries? Nós temos a solução perfeita para você!
+                Recupere seu sorriso com nossos serviços de restauração! Dentes
+                quebrados ou com cáries? Nós temos a solução perfeita para você!
               </p>
               <div className="py-2">
                 <div className="flex items-center gap-2">
                   <FaCheck />
-                  <p className="text-sm my-1">Restaurações com resina composta</p>
+                  <p className="text-sm my-1">Restaurações complexas</p>
                 </div>
                 <div className="flex items-center gap-2">
                   <FaCheck />
-                  <p className="text-sm my-1">Coroas dentárias</p>
+                  <p className="text-sm my-1">Restaurações estéticas</p>
                 </div>
-                <div className="flex items-center gap-2">
-                  <FaCheck />
-                  <p className="text-sm my-1">Implantes dentários</p>
-                </div>
-                <div className="flex items-center gap-2">
-                  <FaCheck />
-                  <p className="text-sm my-1">Facetas de porcelana</p>
-                </div>
+
                 <div className="flex items-center gap-2">
                   <FaCheck />
                   <p className="text-sm my-1">Onlays e inlays</p>
@@ -317,14 +294,153 @@ const Tratamentos = () => {
           </div>
         </div>
 
-        {/* 
-            <div className='flex flex-col items-center justify-center'>
-                <img
-                src={bucomaxilo}
-                alt='Bucomaxilofacial'
-                className='object-cover w-full rounded-lg hover:translate-y-2 duration-500' />
-                <h2 className='text-center my-3 text-xl text-[#7D513A]'>Bucomaxilofacial</h2>
+        <div className="w-full">
+          <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden hover:bg-[#7D513A]/70 hover:text-white transition duration-300 ease-in shadow-lg shadow-gray-400">
+            <img
+              src={bucomaxilo}
+              alt="Bucomaxilofacial"
+              className="object-cover w-full rounded-lg hover:translate-y-2 duration-500"
+            />
+
+            <div className="p-5">
+              <h2 className="text-xl font-medium text-slate-900 mb-1">
+                Bucomaxilofacial
+              </h2>
+              <p className="leading-relaxed">
+                Recupere sua autoestima e qualidade de vida! Oferecemos
+                tratamentos de bucomaxilofacial, como correção de mandíbula e
+                fraturas faciais, com equipe especializada e equipamentos de
+                ponta. Agende sua consulta agora mesmo.
+              </p>
+              <div className="py-2">
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">
+                    Extração de dentes inclusos ou impactados
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">
+                    Correção de deformidades faciais
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">
+                    Tratamento de lesões e doenças na boca, maxilares e face
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">
+                    Cirurgias ortognáticas para correção de problemas na
+                    mandíbula e maxila
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">Tratamento de fraturas faciais</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">Biópsias e remoção de tumores</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">
+                    Tratamento de distúrbios da articulação temporomandibular
+                    (ATM)
+                  </p>
+                </div>
+              </div>
             </div>
+            <div className="px-5 pb-5">
+              <button className="border-2 border-white bg-sky-800 p-3 rounded-lg">
+                <p className="text-white">Agende sua consulta</p>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full">
+          <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden hover:bg-[#7D513A]/70 hover:text-white transition duration-300 ease-in shadow-lg shadow-gray-400">
+            <img
+              src={laserterapia}
+              alt="Laserterapia"
+              className="object-cover w-full rounded-lg hover:translate-y-2 duration-500"
+            />
+
+            <div className="p-5">
+              <h2 className="text-xl font-medium text-slate-900 mb-1">
+                Laserterapia
+              </h2>
+              <p className="leading-relaxed">
+                Alivie a dor e o desconforto da DTM com a laserterapia!
+                Tratamento não invasivo e indolor para relaxar a musculatura e
+                reduzir inflamações. Agende sua sessão agora mesmo.
+              </p>
+              <div className="py-2">
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">Redução de dores e inflamações</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">Relaxamento da musculatura</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">Regeneração tecidual</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">Fotobiomodulação</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">Previne envelhecimento precoce</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">Aumento de imunidade</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">Vasodilatação</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">Alívio do estresse</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">Combate aos radicais livres</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">Remove toxínas</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FaCheck />
+                  <p className="text-sm my-1">Promove hemostasia</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-2">
+                <FaCheck />
+                <p className="text-sm my-1">Melhora na dor articular</p>
+              </div>
+            </div>
+            <div className="px-5 pb-5">
+              <button className="border-2 border-white bg-sky-800 p-3 rounded-lg">
+                <p className="text-white">Agende sua consulta</p>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* 
+            
             <div className='flex flex-col items-center justify-center'>
                 <img
                 src={laserterapia}
